0e00bcbd504307f439f98c9bdd38333d31939d31
stanislav <stas09091994@gmail.com>
2022-12-04 19:13:44 +0300

    The homework file is working

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+using System;
+{
+Console.Write("Введите цифру, обозначающую день недели, соответственно: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckDay (int dayNumber)
+{
+  if (dayNumber == 6 || dayNumber == 7)
+  {
+    Console.WriteLine("Этот день выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7)
+  {
+    Console.WriteLine("Это не день недели");
+  }
+  else Console.WriteLine("Этот день не выходной");
+}
+
+CheckDay(dayNumber);
+
+    Console.ReadKey();
+}
