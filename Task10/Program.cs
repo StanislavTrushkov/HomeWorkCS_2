6ea60fade8d74b90ff84d2da1389aacbea3090a3
stanislav <stas09091994@gmail.com>
2022-12-04 18:59:59 +0300

    The homework file is working

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+using System;
+{
+    Console.Write("Введите трёхзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    string stringNumber = Convert.ToString(num);
+    Console.WriteLine("Вторая цифра числа = "+stringNumber[1]);
+}
